2d3ba73cd02e718435b14aafac56966405f5e636
PukeGuo <guopuke@gmail.com>
2018-03-28 23:56:53 +0800

    array range

--- arrays.go
+++ arrays.go
@@ -8,9 +8,20 @@ func main() {
 
 	array2 := [3]int{1, 3, 5}
 
-	array3 := [...]int{1,2,3,4,5}
+	array3 := [...]int{1, 2, 3, 4, 5}
 
 	var grid [2][3] bool // print [[false false false] [false false false]]
 
-	fmt.Println(array1, array2,array3,grid)
+	fmt.Println(array1, array2, array3, grid)
+
+	printArr(array3)
+	printArr(array1)
+}
+
+// 数组是值类型,调用 func 只会 copy 数组¬
+func printArr(array3 [5]int) {
+	// 循环
+	for i, v := range array3 {
+		fmt.Println(i, v)
+	}
 }
