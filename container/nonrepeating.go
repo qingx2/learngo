b30b154461e9ed4b3f92146b3dc90b4657c6a87b
PukeGuo <guopuke@gmail.com>
2018-04-26 09:56:47 +0800

    Longest substring without repeatiing characters

---
+++ container/nonrepeating.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func lengthOfNonRepeatingSubStr(s string) int {
+	fmt.Println([]byte(s))
+	lastOccurred := make(map[byte]int)
+	start := 0
+	maxLength := 0
+
+	// range 字符串时需要转换类型, 强制转换为 byte 不是英文时会出现问题
+	for i, ch := range []byte(s) {
+		lastI, ok := lastOccurred[ch]
+		// lastOccurred[ch]不存在, 或者 < start , 不会执行
+		// lastOccurred[ch] >= start , 更新 start
+		if ok && lastI >= start {
+			start = lastI + 1
+		}
+		// 更新 lastOccurred[ch], 更新maxLength
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
+
+		fmt.Printf("last=%d\n", lastOccurred[ch])
+		fmt.Printf("start=%d, maxLength=%d\n", start, maxLength)
+	}
+	return maxLength
+}
+
+func main() {
+	fmt.Println(lengthOfNonRepeatingSubStr("abcdddd"))
+	fmt.Println(lengthOfNonRepeatingSubStr("lllllll"))
+	fmt.Println(lengthOfNonRepeatingSubStr("aisjdqoskjsdaq"))
+	fmt.Println(lengthOfNonRepeatingSubStr("abcdddd"))
+	fmt.Println(lengthOfNonRepeatingSubStr(""))
+	fmt.Println(lengthOfNonRepeatingSubStr("abcdef"))
+}
