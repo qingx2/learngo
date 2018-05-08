974317f1b65b262be97f69ffc59a87f02d6bec94
PukeGuo <guopuke@gmail.com>
2018-05-09 00:06:58 +0800

    Add testing script

---
+++ container/nonrepeating/nonrepeating_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+// 测试 - 代码覆盖率 testing.T
+func TestNonrepeating(t *testing.T) {
+	tests := []struct {
+		s   string
+		ans int
+	}{
+		{"abcdddd", 4},
+		{"lllllll", 1},
+		{"", 0},
+		{"我离歌曲", 4},
+		{"吧里bs防化服好", 8},
+	}
+
+	for _, tt := range tests {
+		actual := lengthOfNonRepeatingSubStr(tt.s)
+		if actual != tt.ans {
+			t.Errorf("got %d for input %s; expected %d",
+				actual, tt.s, tt.ans)
+		}
+	}
+}
+
+// 性能测试 testing.B
+func BenchmarkSubstr(b *testing.B) {
+	s := "吧里bs防化服好"
+	ans := 8
+	for i := 0; i < b.N; i++ {
+		actual := lengthOfNonRepeatingSubStr(s)
+		if actual != ans {
+			b.Errorf("got %d for input %s; expected %d",
+				actual, s, ans)
+		}
+	}
+
+}
