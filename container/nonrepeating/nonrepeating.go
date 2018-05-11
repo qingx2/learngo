d15ef62e4763594697a584bba0ee56fe101c21e5
PukeGuo <guopuke@gmail.com>
2018-05-11 23:23:46 +0800

    Testing

--- container/nonrepeating/nonrepeating.go
+++ container/nonrepeating/nonrepeating.go
@@ -2,28 +2,38 @@ package main
 
 import "fmt"
 
+// stores last occurred pos + 1.
+// 0 means not seen
+var lastOccurred = make(map[rune]int)
+
 func lengthOfNonRepeatingSubStr(s string) int {
-	fmt.Println([]byte(s))
-	lastOccurred := make(map[rune]int)
+	// fmt.Println([]byte(s))
+
+	// lastOccurred := make(map[rune]int)
+
+	for i := range lastOccurred {
+		lastOccurred[i] = 0
+	}
+
 	start := 0
 	maxLength := 0
 
 	// range 字符串时需要转换类型, 强制转换为 byte 不是英文时会出现问题
 	for i, ch := range []rune(s) {
-		lastI, ok := lastOccurred[ch]
+		lastI := lastOccurred[ch]
 		// lastOccurred[ch]不存在, 或者 < start , 不会执行
 		// lastOccurred[ch] >= start , 更新 start
-		if ok && lastI >= start {
-			start = lastI + 1
+		if lastI > start {
+			start = lastI
 		}
 		// 更新 lastOccurred[ch], 更新maxLength
 		if i-start+1 > maxLength {
 			maxLength = i - start + 1
 		}
-		lastOccurred[ch] = i
+		lastOccurred[ch] = i + 1
 
-		//fmt.Printf("last=%d\n", lastOccurred[ch])
-		//fmt.Printf("start=%d, maxLength=%d\n", start, maxLength)
+		// fmt.Printf("last=%d\n", lastOccurred[ch])
+		// fmt.Printf("start=%d, maxLength=%d\n", start, maxLength)
 	}
 	return maxLength
 }
--- container/nonrepeating/nonrepeating_test.go
+++ container/nonrepeating/nonrepeating_test.go
@@ -1,9 +1,11 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 // 测试 - 代码覆盖率 testing.T
-func TestNonrepeating(t *testing.T) {
+func TestSubstr(t *testing.T) {
 	tests := []struct {
 		s   string
 		ans int
@@ -27,7 +29,17 @@ func TestNonrepeating(t *testing.T) {
 // 性能测试 testing.B
 func BenchmarkSubstr(b *testing.B) {
 	s := "吧里bs防化服好"
+	for i := 0; i < 2; i++ {
+		s = s +s
+	}
+
+	b.Logf("len(s) = %d", len(s))
+
+	// 上线拼接字符串的时间不算, 重置计时
+	b.ResetTimer()
+
 	ans := 8
+
 	for i := 0; i < b.N; i++ {
 		actual := lengthOfNonRepeatingSubStr(s)
 		if actual != ans {
