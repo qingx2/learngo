544363acb6f67c18f8aec66e1d1cd189174fa9f5
PukeGuo <guopuke@gmail.com>
2018-05-16 18:46:42 +0800

    Atomic safe increment

---
+++ basic/atomic/atomic.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var a atomicInt
+	a.increment()
+	go func() {
+		a.increment()
+	}()
+
+	time.Sleep(time.Millisecond)
+	fmt.Println(a.get())
+}
+
+type atomicInt struct {
+	value int
+	lock  sync.Mutex
+}
+
+func (a *atomicInt) increment() {
+	fmt.Println("Safe increment")
+	func() {
+		a.lock.Lock()
+		defer a.lock.Unlock()
+
+		a.value++
+	}()
+}
+
+func (a *atomicInt) get() int {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+
+	return a.value
+}
