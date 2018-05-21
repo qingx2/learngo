85bfca8ee07a4a96e3bb25234823609eea323282
PukeGuo <guopuke@gmail.com>
2018-05-21 22:53:10 +0800

    Maze algorithm

---
+++ maze/maze.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	maze := readMaze("maze/maze.in")
+
+	// for _, row := range maze {
+	// 	for _, val := range row {
+	// 		fmt.Printf("%d ", val)
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	steps := walk(maze, point{0, 0}, point{len(maze) - 1, len(maze[0]) - 1})
+	for _, row := range steps {
+		for _, val := range row {
+			fmt.Printf("%3d", val)
+		}
+		fmt.Println()
+	}
+}
+
+type point struct {
+	i, j int
+}
+
+var dirs = [4]point{
+	{-1, 0}, {0, -1}, {1, 0}, {0, 1},
+}
+
+func (p point) add(r point) point {
+	return point{p.i + r.i, p.j + r.j}
+}
+
+func (p point) at(grid [][]int) (int, bool) {
+	if p.i < 0 || p.i >= len(grid) {
+		return 0, false
+	}
+
+	if p.j < 0 || p.j >= len(grid[p.i]) {
+		return 0, false
+	}
+
+	return grid[p.i][p.j], true
+}
+
+func walk(maze [][]int, start, end point) [][]int {
+	steps := make([][]int, len(maze))
+	for i := range steps {
+		steps[i] = make([]int, len(maze[i]))
+	}
+	// [[0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0]]
+	// fmt.Println(steps)
+
+	// [{0 0}]
+	Q := []point{start}
+
+	// 队列不为空时, 可继续探索
+	for len(Q) > 0 {
+		cur := Q[0]
+		Q = Q[1:]
+
+		if cur == end {
+			break
+		}
+
+		for _, dir := range dirs {
+			next := cur.add(dir)
+
+			// maze at next is 0
+			// and steps at next is 0
+			// and next != start
+			val, ok := next.at(maze)
+			if !ok || val == 1 {
+				continue
+			}
+
+			val, ok = next.at(steps)
+			if !ok || val != 0 {
+				continue
+			}
+
+			if next == start {
+				continue
+			}
+
+			curSteps, _ := cur.at(steps)
+			steps[next.i][next.j] = curSteps + 1
+
+			Q = append(Q, next)
+		}
+	}
+
+	return steps
+}
+
+func readMaze(filename string) [][]int {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	var row, col int
+	// Get the first line of information , row=6  col=5
+	fmt.Fscanf(file, "%d %d", &row, &col)
+
+	// Create 5 slice. [[] [] [] [] [] []]
+	maze := make([][]int, row)
+	for i := range maze {
+		maze[i] = make([]int, col)
+		for j := range maze[i] {
+			// Fill the data for maze.in
+			fmt.Fscanf(file, "%d", &maze[i][j])
+		}
+	}
+
+	return maze
+}
