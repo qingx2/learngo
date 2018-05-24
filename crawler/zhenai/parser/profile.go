708d1ac7c36c0c359735f4659475249111c7555c
PukeGuo <guopuke@gmail.com>
2018-05-24 23:36:00 +0800

    User infomation parser

---
+++ crawler/zhenai/parser/profile.go
@@ -0,0 +1,65 @@
+package parser
+
+import (
+	"regexp"
+	"strconv"
+
+	"github.com/guopuke/learngo/crawler/engine"
+	"github.com/guopuke/learngo/crawler/model"
+)
+
+var ageRe = regexp.MustCompile(`<td><span class="label">年龄：</span>(\d+)岁</td>`)
+var marriageRe = regexp.MustCompile(`<td><span class="label">婚况：</span>([^<]+)</td>`)
+var heightRe = regexp.MustCompile(`<td><span class="label">身高：</span>(\d+)CM</td>`)
+var weightRe = regexp.MustCompile(`<td><span class="label">体重：</span><span field="">(\d+)KG</span></td>`)
+var educationRe = regexp.MustCompile(`<td><span class="label">学历：</span>([^<]+)</td>`)
+var incomeRe = regexp.MustCompile(`<td><span class="label">月收入：</span>([^<]+)</td>`)
+var hukouRe = regexp.MustCompile(`<td><span class="label">籍贯：</span>([^<]+)</td>`)
+var genderRe = regexp.MustCompile(`<td><span class="label">性别：</span><span field="">([^<]+)</span></td>`)
+var xingzuoRe = regexp.MustCompile(`<td><span class="label">星座：</span><span field="">([^<]+)</span></td>`)
+var occupationRe = regexp.MustCompile(`<td><span class="label">职业：</span><span field="">([^<]+)</span></td>`)
+var carRe = regexp.MustCompile(`<td><span class="label">是否购车：</span><span field="">([^<]+)</span></td>`)
+var houseRe = regexp.MustCompile(`<td><span class="label">住房条件：</span><span field="">([^<]+)</span></td>`)
+
+func ParseProfile(contents []byte, name string) engine.ParseResult {
+	profile := model.Profile{}
+
+	age, err := strconv.Atoi(extractString(contents, ageRe))
+	if err != nil {
+		profile.Age = age
+	}
+	height, err := strconv.Atoi(extractString(contents, heightRe))
+	if err != nil {
+		profile.Height = height
+	}
+	weight, err := strconv.Atoi(extractString(contents, weightRe))
+	if err != nil {
+		profile.Weight = weight
+	}
+
+	profile.Marriage = extractString(contents, marriageRe)
+	profile.Education = extractString(contents, educationRe)
+	profile.Income = extractString(contents, incomeRe)
+	profile.Hukou = extractString(contents, hukouRe)
+	profile.Gender = extractString(contents, genderRe)
+	profile.Xingzuo = extractString(contents, xingzuoRe)
+	profile.Occupation = extractString(contents, occupationRe)
+	profile.Car = extractString(contents, carRe)
+	profile.House = extractString(contents, houseRe)
+
+	result := engine.ParseResult{
+		Items: []interface{}{profile},
+	}
+
+	return result
+}
+
+func extractString(contents []byte, re *regexp.Regexp) string {
+	match := re.FindSubmatch(contents)
+
+	if len(match) >= 2 {
+		return string(match[1])
+	} else {
+		return ""
+	}
+}
